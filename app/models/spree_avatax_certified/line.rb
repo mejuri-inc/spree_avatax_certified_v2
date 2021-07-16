10d6e4a9ec254e60a988a0d2c84dff908a6bab74
Sebastian <delucasd@gmail.com>
2021-07-16 00:26:44 -0300

    [DL-559] Updating returns transaction so the amount does not include taxes amount to avoid recalculation.

--- app/models/spree_avatax_certified/line.rb
+++ app/models/spree_avatax_certified/line.rb
@@ -117,7 +117,7 @@ module SpreeAvataxCertified
         :TaxCode => line_item.tax_category.try(:description) || 'P0000000',
         :ItemCode => line_item.variant.sku,
         :Qty => quantity,
-        :Amount => -amount.to_f,
+        :Amount => -amount + return_line_item_taxes(line_item, quantity),
         :OriginCode => stock_location,
         :DestinationCode => 'Dest',
         :CustomerUsageType => order.user ? order.user.avalara_entity_use_code.try(:use_code) : ''
@@ -157,6 +157,11 @@ module SpreeAvataxCertified
 
     def customer_usage_type
       order.user ? order.user.avalara_entity_use_code.try(:use_code) : ''
+    end
+
+    def return_line_item_taxes(line_item, quantity)
+      total_tax_amount = line_item.additional_tax_total + line_item.included_tax_total
+      total_tax_amount * (quantity.to_f / line_item.quantity)
     end
   end
 end
